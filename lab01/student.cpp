914a827969ef01534a1cf962765afb1f1d3a55bd
钱广辉 <43242860+JustinQuan777@users.noreply.github.com>
2018-10-12 23:37:44 +0800

    增加注释

--- lab01/student.cpp
+++ lab01/student.cpp
@@ -1,12 +1,12 @@
 #include "student.h"
 #include<QDebug>
 
-student::student()
+student::student()                                                                            //构造函数
 {
 
 }
 
-void student::Set(int num,QString nam, int sub1, int sub2)
+void student::Set(int num,QString nam, int sub1, int sub2)                                   //实现每个对象赋值
 {
     number=num;
     name=nam;
@@ -15,18 +15,18 @@ void student::Set(int num,QString nam, int sub1, int sub2)
 
 }
 
-void student::Printfirst()
+void student::Printfirst()                                                                    //输出表头
 {
     qDebug()<<"\t"<<"\t"<<"学号"<<"\t"<<"姓名"<<"\t"<<"\t"<<"课程1"<<"\t"<<"课程2";
 
 }
 
-void student::Print()
+void student::Print()                                                                         //输出每位学生的信息
 {
     qDebug()<<"\t"<<number<<"\t"<<name<<"\t"<<subject1<<"\t"<<subject2;
 }
 
-bool student::name_sort(const student &t1, const student &t2)
+bool student::name_sort(const student &t1, const student &t2)                                 //实现姓名比较
 {
     if (t1.name>t2.name)
         return true;
@@ -34,7 +34,7 @@ bool student::name_sort(const student &t1, const student &t2)
         return false;
 }
 
-bool student::sub1_sort(const student &t1, const student &t2)
+bool student::sub1_sort(const student &t1, const student &t2)                                //实现课程1比较
 {
     if (t1.subject1>t2.subject1)
         return true;
@@ -42,7 +42,7 @@ bool student::sub1_sort(const student &t1, const student &t2)
         return false;
 }
 
-bool student::sub2_sort(const student &t1, const student &t2)
+bool student::sub2_sort(const student &t1, const student &t2)                                //实现课程2比较
 {
     if (t1.subject2>t2.subject2)
         return true;
